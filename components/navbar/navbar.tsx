491b4366e4da96ab244144ae636f69e71a5363a7
Benex Drake <benex@hotmail.de>
2023-11-13 14:13:41 +0100

    initial new portfolio project

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar()
+{
+
+
+    return (
+        <nav>HELLO NAVBAR</nav>
+    )
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/navbar/navbar'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -5,8 +6,11 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <Navbar/>
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossOrigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/74eb824b54.js" crossOrigin="anonymous"></script>
       </body>
     </Html>
   )
